298bbfda32b86f7471807550e9abcc67601f1fcc
André Keher <andrekeher@aprendiz.org.br>
2019-09-23 15:24:57 -0300

    Initial commit

---
+++ src/Columns.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace AndreKeher\WPDP;
+
+class Columns
+{
+    private $postType;
+    private $columns;
+    private $dataFunction;
+    
+    public function __construct($postType)
+    {
+        $this->postType = $postType;
+        $this->columns = [
+            'cb' => '<input type="checkbox" />',
+            'title' => __('Title'),
+            'author' => __('Author'),
+            'categories' => __('Categories'),
+            'tags' => __('Tags'),
+            'comments' => __('<span class="vers comment-grey-bubble" title="' . esc_attr__('Comments') . '"><span class="screen-reader-text">' . __('Comments') . '</span></span>'),
+            'date' => __('Date'),
+        ];
+    }
+    
+    public function getColumns()
+    {
+        return $this->columns;
+    }
+    
+    public function setColumns($columns = [])
+    {
+        $this->columns = $columns;
+    }
+    
+    public function appendColumn($column = [])
+    {
+        $this->columns = array_merge($this->columns, $column);
+    }
+    
+    public function removeColumn($key)
+    {
+        if (isset($this->columns[$key])) {
+            unset($this->columns[$key]);
+        }
+    }
+    
+    public function setDataFunction(callable $function)
+    {
+        $this->dataFunction = $function;
+    }
+    
+    public function filterColumns($columns)
+    {
+        return $this->columns;
+    }
+    
+    public function init()
+    {
+        if (empty($this->dataFunction)) {
+            wp_die(__('Please, add the data function.'));
+        }
+        add_filter(sprintf('manage_%s_posts_columns', $this->postType), array($this, 'filterColumns'));
+        add_action(sprintf('manage_%s_posts_custom_column', $this->postType), $this->dataFunction, 10, 2);
+    }
+}
